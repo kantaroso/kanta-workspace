c8db6161ce7417c50b9fceb29f0dedde08ec5951
kantaroso <dev.midori.erika.0607@gmail.com>
2020-03-22 17:24:16 +0900

    つまづいた部分にコメントを追記

--- app/gin/controllers/top.go
+++ app/gin/controllers/top.go
@@ -9,6 +9,8 @@ import (
 
 // Top 表紙のページを表示する
 func Top(c *gin.Context, router *gin.Engine) {
+	// template の layout を利用する
+	// https://qiita.com/taizo/items/38e4af19e2f42208a00a
 	html := template.Must(template.ParseFiles("templates/layouts/base.html", "templates/contents/index.html"))
 	router.SetHTMLTemplate(html)
 	c.HTML(http.StatusOK, "base.html", gin.H{})
--- app/gin/main.go
+++ app/gin/main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	// controller の 処理はモジュール化
+	// https://qiita.com/tkj06/items/a5f79417935100045650
 	"github.com/kantaroso/kanta-workspace/controllers"
 )
 
 func main() {
 	router := gin.Default()
+	// gin teemplateの使い方
+	// https://qiita.com/lanevok/items/dbf591a3916070fcba0d
 	router.LoadHTMLGlob("templates/**/*.html")
 
+	// 外部のパッケージの何かを呼び出す時は文頭を大文字にする
+	// https://qiita.com/ko-watanabe/items/875085780d2ad72fe6af
 	router.GET("/", func(c *gin.Context) { controllers.Top(c, router) })
 
 	router.Run()
